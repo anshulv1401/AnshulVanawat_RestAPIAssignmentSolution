471f22f5027a3462e652819e705c4238c1c306d9
anshulv1401 <anshulv1401@gmail.com>
2021-12-22 00:59:00 +0530

    General-Server-Dev-Apis for Roles added

--- EmployeeManagementSystem/src/main/java/com/greatlearning/ems/controller/RoleController.java
+++ EmployeeManagementSystem/src/main/java/com/greatlearning/ems/controller/RoleController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,53 +29,23 @@ public class RoleController {
 
 	@PostMapping()
 	public String post(@RequestBody() RoleDto role) {
-
-		var roleFromDB = roleService.findByName(role.getName());
-		if (roleFromDB.isPresent()) {
-			return String.format("Role %s already exists", role.getName());
-		}
 		roleService.save(new Role(role.getName()));
 		return String.format("Role %s Saved Successfully", role.getName());
 	}
 
-	@PutMapping
-	public Role put(@RequestParam int theId, @RequestBody String name) {
-
-		var roleFromDB = roleService.findById(theId);
-
-		if (roleFromDB.isPresent()) {
-			var newRole = roleFromDB.get();
-			newRole.setName(name);
-			roleService.save(newRole);
-			return newRole;
-		} else {
-			var newRole = new Role(name);
-			roleService.save(newRole);
-			return newRole;
-		}
+	@DeleteMapping("{id}")
+	public String delete(@PathVariable int id) {
+		roleService.deleteById(id);
+		return String.format("Role with id %s Deleted Successfully", id);
 	}
 
-	@DeleteMapping
-	public String delete(@RequestParam("id") int theId) {
-		var roleFromDB = roleService.findById(theId);
-		if (roleFromDB.isEmpty()) {
-			return String.format("Role with id %s does not exists", theId);
-		}
-		roleService.deleteById(theId);
-		return String.format("Role with id %s Deleted Successfully", theId);
+	@GetMapping("{id}")
+	public Role get(@PathVariable int id) {
+		return roleService.findById(id).get();
 	}
 
-	@GetMapping
-	public List<Role> get(Optional<Integer> id) {
-
-		if (id.isEmpty())
-			return roleService.findAll();
-		else {
-			List<Role> roles = new ArrayList<Role>();
-			var role = roleService.findById(id.get());
-			if(role.isPresent())
-				roles.add(role.get());
-			return roles;
-		}
+	@GetMapping()
+	public List<Role> getAll() {
+		return roleService.findAll();
 	}
 }
