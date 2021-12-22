99c091aecb2f239436fa0c5a9bb61c0d62f748b7
anshulv1401 <anshulv1401@gmail.com>
2021-12-22 23:50:16 +0530

    General-Server-Dev-Security Added to all APIs

---
+++ EmployeeManagementSystem/src/main/java/com/greatlearning/ems/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.greatlearning.ems.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+	@GetMapping
+	public String get() {
+		return "Welcome to Employee Management System";
+	}
+	
+	@GetMapping("accessDenied")
+	public String accessDenied() {
+		return "Access Denied";
+	}
+}
